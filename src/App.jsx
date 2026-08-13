5e31d7fec8c9b2b124cb61551fb5974e75b4c449
jihtin <129176693+jihtin@users.noreply.github.com>
2026-08-13 09:45:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+ <h1  className="bg-blue-800 p-12 text-amber-600 " >hii</h1>
+    </>
+  )
+}
+
+export default App
+
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+          <li>Create task</li>
+          <li>Task list</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
